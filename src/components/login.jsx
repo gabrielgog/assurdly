afb233abc4cdf155f303b277a3eb78eb95659fc2
Gabriel Godwin <gabrielgog96@gmail.com>
2020-12-11 17:43:47 +0100

    add login button

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -5,6 +5,14 @@ function Login(){
     return<div>
 
        <h1>Welcome to Asurdly</h1> 
+
+       <form action="">
+           <input type="text" name="" placeholder="First Name" id=""/>
+
+           <input type="text" placeholder="Last Name" name="" id=""/>
+
+           <button>Login</button>
+       </form>
     </div>
 }
 
